174b613184852c8f940a2f7288f4f7e4d9bb86a6
thisisaaronland <straup@gmail.com>
2018-08-08 19:50:42 +0000

    update validate tool to use travel package

--- cmd/lieu-validate.go
+++ cmd/lieu-validate.go
@@ -1,109 +1,14 @@
 package main
 
 import (
-	"bufio"
-	"compress/bzip2"
-	"context"
 	"encoding/json"
 	"flag"
-	"github.com/whosonfirst/go-whosonfirst-lieu"
+	"github.com/whosonfirst/go-whosonfirst-lieu/travel"
 	"io"
 	"log"
 	"os"
-	"path/filepath"
-	"runtime"
 	"strings"
-	"time"
 )
-
-func ValidateFile(path string, throttle_ch chan bool) error {
-
-	t1 := time.Now()
-
-	defer func() {
-		log.Printf("time to validate %s %v\n", path, time.Since(t1))
-	}()
-
-	var r io.Reader
-
-	fh, err := os.Open(path)
-
-	if err != nil {
-		return err
-	}
-
-	r = fh
-
-	if filepath.Ext(path) == ".bz2" {
-		br := bufio.NewReader(fh)
-		r = bzip2.NewReader(br)
-	}
-
-	scanner := bufio.NewScanner(r)
-	lineno := 0
-
-	done_ch := make(chan bool)
-	err_ch := make(chan error)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	for scanner.Scan() {
-
-		// <- throttle_ch
-		lineno += 1
-
-		doc := scanner.Text()
-
-		go validateDocument(ctx, doc, lineno, throttle_ch, done_ch, err_ch)
-	}
-
-	remaining := lineno
-
-	for remaining > 0 {
-
-		select {
-		case <-done_ch:
-			// throttle_ch <- true
-			remaining -= 1
-		case e := <-err_ch:
-			log.Println(e)
-		default:
-			// pass
-		}
-	}
-
-	return nil
-}
-
-func validateDocument(ctx context.Context, doc string, lineno int, throttle_ch chan bool, done_ch chan bool, err_ch chan error) {
-
-	defer func() {
-		done_ch <- true
-		throttle_ch <- true
-	}()
-
-	<-throttle_ch
-
-	select {
-	case <-ctx.Done():
-		return
-	default:
-		// pass
-	}
-
-	err := ensureValidJSON(doc)
-
-	if err != nil {
-		err_ch <- err
-		return
-	}
-
-	if lieu.HasPhone([]byte(doc)) && !lieu.HasCountry([]byte(doc)) {
-		ph, _ := lieu.GetPropertyByKey([]byte(doc), "phone")
-		log.Println("phone and country", ph.Value.String())
-	}
-}
 
 func ensureValidJSON(doc string) error {
 
@@ -130,20 +35,15 @@ func ensureValidJSON(doc string) error {
 
 func main() {
 
-	var procs = flag.Int("processes", runtime.NumCPU()*2, "The number of concurrent processes to use")
-	// var strict = flag.Bool("strict", false, "Whether or not to trigger a fatal error when invalid JSON is encountered")
-	// var stats = flag.Bool("stats", false, "Be chatty, with counts and stuff")
-
 	flag.Parse()
 
-	throttle_ch := make(chan bool, *procs)
-
-	for i := 0; i < *procs; i++ {
-		throttle_ch <- true
-	}
-
 	for _, path := range flag.Args() {
-		ValidateFile(path, throttle_ch)
+
+		err := travel.Travel(path, ensureValidJSON)
+
+		if err != nil {
+			log.Fatal(path, err)
+		}
 	}
 
 	os.Exit(0)
